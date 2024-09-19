3f29d25e381a2322e7d80cfcdd145119cabce91d
Gabriel Arbigaus <gabriel.arbigaus@stratesys-ts.com>
2024-09-19 03:13:46 -0300

    Criando um projeto React e criando o primeiro componente

---
+++ curso-react/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from './components/button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button/>
+    </div>
+  );
+}
+
+export default App;
