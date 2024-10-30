a6e9fab1e3bbf44613c1594315c79ec558ffd3f6
Gabriel Arbigaus <gabriel.arbigaus@stratesys-ts.com>
2024-10-30 00:51:49 -0300

    (CAP 07 / AULA 02) Adicionando método useEffect para corrigir o erro do commit passado para que o tempo da task selecionado seja salvo

--- curso-react/src/components/Stopwatch/stopwatch.tsx
+++ curso-react/src/components/Stopwatch/stopwatch.tsx
@@ -1,19 +1,22 @@
 import Button from "../Button/button";
 import Watch from "./Watch/watch";
-import style from "./Stopwatch.module.scss"
+import style from "./Stopwatch.module.scss";
 import { timeToSeconds } from "../../common/utils/time";
 import { ITask } from "../../types/task";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface Props{
     selected: ITask | undefined
 }
 
 export default function Stopwatch({selected}: Props){
+
+    useEffect(()=>{
+        if (selected?.time) {
+            setTime(timeToSeconds(String(selected?.time)))
+        }
+    },[selected]);
     const [time, setTime] = useState<number>();
-    if (selected?.time) {
-        setTime(timeToSeconds(selected?.time));
-    }
     return(
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronomêtro</p>
